1d03e924c11350a4687979d9e92119097cb37572
PieStw <piero.stalteri@icloud.com>
2024-12-14 13:19:26 +0100

    creazione contesto

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,26 @@ import AboutUsPage from "./pages/AboutUsPage";
 import PostPage from "./pages/PostPage";
 import DefaultLayout from "./layouts/DefaultLayout";
 import DetailPage from "./pages/DetailPage";
+import Contex from "./contex/Contex";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route Component={DefaultLayout}>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/aboutus" element={<AboutUsPage />}></Route>
-            <Route path="/postpage" element={<PostPage />}></Route>
-            <Route path="/postpage/detail/:id" element={<DetailPage />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Contex>
+        <BrowserRouter>
+          <Routes>
+            <Route Component={DefaultLayout}>
+              <Route path="/" element={<HomePage />}></Route>
+              <Route path="/aboutus" element={<AboutUsPage />}></Route>
+              <Route path="/postpage" element={<PostPage />}></Route>
+              <Route
+                path="/postpage/detail/:id"
+                element={<DetailPage />}
+              ></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Contex>
     </>
   );
 }
